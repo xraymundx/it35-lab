9f25666de9381ff997c176ce9a26fe4e0025976f
xraymundx <raymundmacapundag113@gmail.com>
2025-02-20 16:08:38 +0800

    Updates

--- assets/swipe-back-legacy-CrDh6hmq.js
+++ assets/swipe-back-legacy-CrDh6hmq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHHfhFzk.js","./index3-legacy-Cz81LQVf.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DvLzXs_m.js","./index3-legacy-BzdF-kX9.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
