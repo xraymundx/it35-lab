cb720d14d80330e6df719644ffe3e096e0511ec5
xraymundx <raymundmacapundag113@gmail.com>
2025-02-27 14:32:21 +0800

    Updates

--- assets/ios.transition-legacy-CrAgX7Kt.js
+++ assets/ios.transition-legacy-CrAgX7Kt.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DvLzXs_m.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-DqcthdSQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-COocvurb.js
+++ assets/swipe-back-legacy-COocvurb.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DvLzXs_m.js","./index3-legacy-BzdF-kX9.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-DqcthdSQ.js","./index3-legacy-DRuX_Gk5.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
