cfd4d361bafdcaa723c21436cdd3c331effb3021
xraymundx <raymundmacapundag113@gmail.com>
2025-03-21 09:14:24 +0800

    added function to handle registration

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ const saveUser = (user) => {
     users.push(user);
     localStorage.setItem('users', JSON.stringify(users));
 };
+
+function handleRegistration(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('regUsername').value;
+    const email = document.getElementById('regEmail').value;
+    const password = document.getElementById('regPassword').value;
+    const confirmPassword = document.getElementById('regConfirmPassword').value;
+
+
+}
