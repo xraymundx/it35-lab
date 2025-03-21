5023c10eb51ec283adf2b1f96b44742db67c044c
xraymundx <raymundmacapundag113@gmail.com>
2025-03-21 09:24:36 +0800

    added function to toggle form

--- script.js
+++ script.js
@@ -43,4 +43,15 @@ function handleRegistration(event) {
     return false;
 }
 
-
+function toggleForms() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+    
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
+    } else {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+    }
+}
