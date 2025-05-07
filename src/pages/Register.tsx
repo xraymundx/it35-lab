75ef73d07d7111fb7900f31bf01ac58ea41cda4f
xraymundx <raymudmacapundag113@gmail.com>
2025-05-07 18:09:41 +0800

    added method and form for registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -13,10 +13,42 @@ import {
       IonToolbar,
       useIonRouter
   } from '@ionic/react';
-
+  
   const Register: React.FC = () => {
-    const navigation = useIonRouter();
-    const Register = () => {
-       navigation.push('/it35-lab', 'forward', 'replace');
-     }
-    }
+     const navigation = useIonRouter();
+     const Register = () => {
+        navigation.push('/it35-lab', 'forward', 'replace');
+      }
+
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            {/* <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons> */}
+            <IonTitle>Register</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+            <IonItem>
+        <IonTextarea label="First Name" labelPlacement="floating" fill="solid" placeholder="Enter text"></IonTextarea>
+        </IonItem>
+        <IonItem>
+        <IonTextarea label="Last Name" labelPlacement="floating" fill="solid" placeholder="Enter text"></IonTextarea>
+        </IonItem>
+        <IonItem>
+        <IonTextarea label="Address" labelPlacement="floating" fill="solid" placeholder="Enter text"></IonTextarea>
+        </IonItem>
+        <IonTextarea label="Email" labelPlacement="floating" fill="solid" placeholder="Enter text"></IonTextarea>
+        <IonItem>
+        <IonInput label="Password" labelPlacement="floating" fill="solid" type="password" placeholder="Enter password"/>
+        </IonItem>
+        <IonItem>
+        <IonInput label="Confirm Password" labelPlacement="floating" fill="solid" type="password" placeholder="Confirm password"/>
+        </IonItem>
+        <IonButton onClick={Register} expand="full">Register</IonButton>
+        </IonContent>
+      </IonPage>
+    );
+  };
