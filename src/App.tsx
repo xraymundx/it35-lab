7b3eeed9c6273702ec52fd1e7c6f21072d052ebf
xraymundx <raymudmacapundag113@gmail.com>
2025-05-07 18:13:51 +0800

    added import for registration

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 import './theme/variables.css';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
+import Register from './pages/Register';
 
 setupIonicReact();
 
