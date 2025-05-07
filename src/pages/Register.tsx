f81ec1d5709c5112f2ae4c457ac1cc6f64b52d8f
xraymundx <raymudmacapundag113@gmail.com>
2025-05-07 18:05:13 +0800

    added function to registration

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -13,3 +13,10 @@ import {
       IonToolbar,
       useIonRouter
   } from '@ionic/react';
+
+  const Register: React.FC = () => {
+    const navigation = useIonRouter();
+    const Register = () => {
+       navigation.push('/it35-lab', 'forward', 'replace');
+     }
+    }
