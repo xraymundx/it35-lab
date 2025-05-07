b3c9436b17852dd8a5860c8ff5521dd3764d5bff
xraymundx <raymudmacapundag113@gmail.com>
2025-05-07 17:30:16 +0800

    added the components of login and menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -32,19 +32,18 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from './pages/Login';
+import Menu from './pages/Menu';
 
 setupIonicReact();
+
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/it35-lab/home">
-          <Home />
-        </Route>
-        <Route exact path="/it35-lab/">
-          <Redirect to="/it35-lab/home" />
-        </Route>
+      <Route exact path="/it35-lab" component={Login} />
+      <Route  path="/it35-lab/app" component={Menu} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
