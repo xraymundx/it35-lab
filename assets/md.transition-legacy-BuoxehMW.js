1977e15777d0fc5452cabe99e591ba239e69aa22
xraymundx <raymudmacapundag113@gmail.com>
2025-05-07 10:41:41 +0800

    Updates

--- assets/md.transition-legacy-BuoxehMW.js
+++ assets/md.transition-legacy-BuoxehMW.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DpkbDGcE.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-B0wSu2Uz.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
