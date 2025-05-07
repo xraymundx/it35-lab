098ec644fe20dcf63272a4cce0444955c2f2b149
xraymundx <raymudmacapundag113@gmail.com>
2025-05-07 10:24:01 +0800

    added menu.tsx and imports

---
+++ src/pages/Menu.tsx
@@ -0,0 +1,16 @@
+import { 
+    IonButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonIcon, 
+      IonItem, 
+      IonMenu, 
+      IonMenuButton, 
+      IonMenuToggle, 
+      IonPage, 
+      IonRouterOutlet, 
+      IonSplitPane, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react'
