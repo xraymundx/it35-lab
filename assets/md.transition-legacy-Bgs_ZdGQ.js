bd376ed931d0e5e77a12f25c019c240335c2cbec
xraymundx <raymudmacapundag113@gmail.com>
2025-05-08 17:02:25 +0800

    Updates

--- assets/md.transition-legacy-Bgs_ZdGQ.js
+++ assets/md.transition-legacy-Bgs_ZdGQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DSwGJ0Ry.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DTGhLDZD.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
