4f733df980c0bc0c9e02096bc2f09da8011e76c7
xraymundx <raymudmacapundag113@gmail.com>
2025-05-12 00:14:19 +0800

    Updates

--- assets/ios.transition-legacy-DiH5ZAN4.js
+++ assets/ios.transition-legacy-DiH5ZAN4.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CWxN9T_F.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BHkb73JQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
